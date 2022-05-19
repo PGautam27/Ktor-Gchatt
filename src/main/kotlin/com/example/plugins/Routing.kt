259eaaa1ff208ac032c25a9fec334b673f60bcda
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-19 17:43:55 +0530

    Ktor Gchatt app

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,16 @@
 package com.example.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import com.example.room.RoomController
+import com.example.route.chatSocket
+import com.example.route.getAllMessages
+import io.ktor.application.*
+import io.ktor.routing.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
-
-//    routing {
-//        get("/") {
-//            call.respondText("Hello World!")
-//        }
-//    }
+    val roomController by inject<RoomController>()
+    install(Routing) {
+        chatSocket(roomController)
+        getAllMessages(roomController)
+    }
 }
