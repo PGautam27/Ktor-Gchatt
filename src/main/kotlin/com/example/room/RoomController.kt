ab95b1957639d7e53e2f97b2a4258039240dfc14
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-19 15:25:12 +0530

    Ktor Gchatt app

--- src/main/kotlin/com/example/room/RoomController.kt
+++ src/main/kotlin/com/example/room/RoomController.kt
@@ -2,9 +2,12 @@ package com.example.room
 
 import com.example.data.MessageDataSource
 import com.example.data.model.Message
-import io.ktor.http.*
 import io.ktor.websocket.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.collections.forEach
+import kotlin.collections.set
 
 class RoomController(
     private val messageDataSource: MessageDataSource
@@ -26,14 +29,19 @@ class RoomController(
         )
     }
 
-    fun sendMessage(senderUsername: String, message: String){
+    suspend fun sendMessage(senderUsername: String, message: String){
         member.values.forEach{member->
             val messageEntity = Message(
                 text = message,
                 username = senderUsername,
                 timestamp = System.currentTimeMillis()
             )
-            messageDataSource.
+            messageDataSource.insertMessage(messageEntity)
+
+            val parsedMessage = Json.encodeToString(messageEntity)
+            member.socket.send(Frame.Text(parsedMessage))
+
+
         }
     }
 }
